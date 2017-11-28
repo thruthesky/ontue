f243d8f88b03b54b481b089dbd06ca22408bfad0
lancelynyrd <pinedaclp@gmail.com>
2017-11-28 16:13:02 +0800

    reservation

--- src/pages/reservation/reservation.ts
+++ src/pages/reservation/reservation.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { AppService } from './../../providers/app.service';
-import {Subject} from "rxjs/Subject";
 
 @Component({
   selector: 'page-reservation',
@@ -10,44 +9,42 @@ export class ReservationPage {
   books = [];
   my_teachers = [];
 
+
   my_point;
 
-  
+  future_class:boolean = false;
+  past_class:boolean = false;
 
 
   date_begin = null;
   date_end = null;
 
-  private typing = new Subject<string>();
 
   constructor(public a: AppService) {
     this.a.loadMyPoint( p => this.my_point = p );
 
     this.sessionSearch(this.request());
     this.updatePoint();
-    this.typing
-      .debounceTime(300)
-      .subscribe(() => {
-        this.onChangeSearchOption();
-      });
 
-
-      
   }
-
 
   request( options = {} ) {
     let defaults = {
-      // future: true,
       // date_begin: '20171201',
       // date_end: '20171201',
       orderby: 'date ASC, class_begin ASC'
     };
+
+    if(this.future_class != this.past_class ) {
+      if( this.future_class ) defaults['future']= this.future_class;
+      if( this.past_class ) defaults['past']= this.past_class;
+    }
+    if( this.date_begin ) defaults['date_begin']= this.date_begin.replace(/\-/g, '');
+    if( this.date_end ) defaults['date_end']= this.date_end.replace(/\-/g, '');
     const req = Object.assign( defaults, options );
     console.log("Request: ", req );
     return req;
   }
-
 
   onClickCancelAll() {
     this.books.map( book => this.onClickCancel( book ) );
@@ -62,14 +59,12 @@ export class ReservationPage {
 
   }
 
-
   updatePoint() {
 
     this.a.loadMyPoint( p => this.my_point = p );
 
 
   }
-
 
   onChangeSearchOption() {
     this.sessionSearch( this.request() );
@@ -84,19 +79,8 @@ export class ReservationPage {
 
   }
 
-
-  onChangeValue() {
-    this.typing.next();
-  }
-
-
-  
   onClickSearch() {
-    let date_begin = this.date_begin.replace(/\-/g, '');
-    let date_end = this.date_begin.replace(/\-/g, '');
-
-
-    this.a.lms.session_search( { date_begin: date_begin, date_end: date_end } ).subscribe( re => {
+    this.a.lms.session_search( this.request() ).subscribe( re => {
       console.log(re);
     }, e => this.a.alert(e) );
   }
