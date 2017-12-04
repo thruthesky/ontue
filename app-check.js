ebeb443a5429cccfca36a3e229c012d4f33c04e5
JaeHo Song <thruthesky@gmail.com>
2017-12-04 23:57:27 +0800

    update

--- app-check.js
+++ app-check.js
@@ -4,12 +4,12 @@ var fs = require('fs');
 var contents = fs.readFileSync('src/index.html').toString();
 
 if ( contents.indexOf("// if ('serviceWorker' in navigator) {") == -1 ) {
-    console.log("WARNING:\nPlease uncomment pwa code in index.html\n");
+    console.log("WARNING:\nPlease Disable pwa code in index.html\n");
     process.exit(1);
 }
 
-if ( contents.indexOf('<!-- <script src="cordova.js"></script> -->') == -1 ) {
-    console.log("WARNING:\nPlease comment out cordova script tag in index.html\n");
+if ( contents.indexOf('<!-- <script src="cordova.js"></script> -->') != -1 ) {
+    console.log("WARNING:\nPlease Enable cordova script tag in index.html\n");
     process.exit(2);
 }
 
@@ -17,21 +17,21 @@ if ( contents.indexOf('<!-- <script src="cordova.js"></script> -->') == -1 ) {
 var fs = require('fs');
 var app_component = fs.readFileSync('src/app/app.component.ts').toString();
 if ( app_component.indexOf('// platform.ready().then(() => {') != -1 ) {
-    console.log("WARNING:\nPlease comment out patform.ready() in app.component.ts\n");
+    console.log("WARNING:\nPlease Enable patform.ready() in app.component.ts\n");
     process.exit(2);
 }
 
 if ( app_component.indexOf("// import { SplashScreen } from '@ionic-native/splash-screen';") != -1 ) {
-    console.log("WARNING:\nPlease Uncomment SplashSreen in app.component.ts\n");
+    console.log("WARNING:\nPlease Eanble SplashSreen in app.component.ts\n");
     process.exit(2);
 }
 
 if ( app_component.indexOf("//   statusBar.styleDefault();") != -1 ) {
-    console.log("WARNING:\nPlease Uncomment statusBar.styleDefault(); in app.component.ts\n");
+    console.log("WARNING:\nPlease Eanble statusBar.styleDefault(); in app.component.ts\n");
     process.exit(2);
 }
 if ( app_component.indexOf("//   splashScreen.hide();") != -1 ) {
-    console.log("WARNING:\nPlease Uncomment //   splashScreen.hide(); in app.component.ts\n");
+    console.log("WARNING:\nPlease Eanble //   splashScreen.hide(); in app.component.ts\n");
     process.exit(2);
 }
 
@@ -42,20 +42,20 @@ if ( app_component.indexOf("//   splashScreen.hide();") != -1 ) {
 var fs = require('fs');
 var app_module = fs.readFileSync('src/app/app.module.ts').toString();
 if ( app_component.indexOf("// import { StatusBar } from '@ionic-native/status-bar';") != -1 ) {
-    console.log("WARNING:\nPlease Uncomment StatusBar in app.component.ts\n");
+    console.log("WARNING:\nPlease Eanble StatusBar in app.component.ts\n");
     process.exit(2);
 }
 
 
 
 if ( app_module.indexOf("// import { SplashScreen } from '@ionic-native/splash-screen';") != -1 ) {
-    console.log("WARNING:\nPlease Uncomment SplashSreen in app.module.ts\n");
+    console.log("WARNING:\nPlease Eanble SplashSreen in app.module.ts\n");
     process.exit(2);
 }
 
 
 if ( app_module.indexOf("// import { StatusBar } from '@ionic-native/status-bar';") != -1 ) {
-    console.log("WARNING:\nPlease Uncomment StatusBar in app.module.ts\n");
+    console.log("WARNING:\nPlease Eanble StatusBar in app.module.ts\n");
     process.exit(2);
 }
 
@@ -63,12 +63,12 @@ if ( app_module.indexOf("// import { StatusBar } from '@ionic-native/status-bar'
 var fs = require('fs');
 var app_module = fs.readFileSync('src/app/app.module.ts').toString();
 if ( app_module.indexOf("// StatusBar,") != -1 ) {
-    console.log("WARNING:\nPlease Uncomment StatusBar in Providers in app.module.ts\n");
+    console.log("WARNING:\nPlease Eanble StatusBar in Providers in app.module.ts\n");
     process.exit(2);
 }
 var app_module = fs.readFileSync('src/app/app.module.ts').toString();
 if ( app_module.indexOf("// SplashScreen,") != -1 ) {
-    console.log("WARNING:\nPlease Uncomment SplashScreen in Providers in app.module.ts\n");
+    console.log("WARNING:\nPlease Eanble SplashScreen in Providers in app.module.ts\n");
     process.exit(2);
 }
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { Platform, Nav } from 'ionic-angular';
-// import { StatusBar } from '@ionic-native/status-bar';
-// import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 
 import { XapiService } from './../angular-xapi/angular-xapi.module';
@@ -46,21 +46,21 @@ export class MyApp {
 
   constructor(
     platform: Platform,
-    // statusBar: StatusBar,
-    // splashScreen: SplashScreen,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
     xapi: XapiService,
     public a: AppService
   ) {
       this.a.initTranslate();
 
-    // platform.ready().then(() => {
-    //   // Okay, so the platform is ready and our plugins are available.
-    //   // Here you can do any higher level native things you might need.
-    //   statusBar.styleDefault();
-    //   splashScreen.hide();
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
 
-    //   // alert("I am on cordova!");
-    // });
+      // alert("I am on cordova!");
+    });
 
   }
 
@@ -128,8 +128,7 @@ export class MyApp {
 
     // this.a.open('evaluate', {idx: 1639} );
 
-
-    // this.a.open('settings-payment-info');
+    this.a.open('settings-payment-info');
 
     // this.a.open('payment');
     
