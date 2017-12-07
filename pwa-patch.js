94886be99ffef9c85a8f481aa1afae5b1d92caa8
JaeHo Song <thruthesky@gmail.com>
2017-12-07 16:24:03 +0800

    update for pwa

--- pwa-patch.js
+++ pwa-patch.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var d = new Date();
-var hash = d.getFullYear() + d.getMonth() + d.getDay() + '-' + d.getHours() + d.getMinutes() + d.getSeconds();
+var hash = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDay() + '-' + d.getHours() +'-'+ d.getMinutes() +'-'+ d.getSeconds();
 var new_main_js = 'main-' + hash + '.js';
 fs.rename('www/build/main.js', 'www/build/' + new_main_js, function(err) {
     if ( err ) console.log('ERROR: ' + err);
@@ -12,10 +12,32 @@ fs.rename('www/build/main.css', 'www/build/' + new_main_css , function(err) {
 });
 
 
+
+/// Delete old service-worker.xxxxxx.js
+var files = fs.readdirSync('www');
+for ( let file of files ) {
+    if ( file.indexOf('service-worker-') != -1 ) {
+        var path = 'www/' + file;
+        console.log('unlink: ', path);
+        fs.unlinkSync( path );
+    }
+}
+
+
+
+var content = fs.readFileSync('www/service-worker.js').toString();
+content = content.replace('main.js', new_main_js);
+content = content.replace('main.css', new_main_css);
+fs.writeFileSync('www/service-worker.js', content);
+
 var new_service_worker = 'service-worker-'+ hash +'.js';
-fs.rename('www/build/main.css', 'www/build/' + new_main_css , function(err) {
+fs.rename('www/service-worker.js', 'www/' + new_service_worker , function(err) {
     if ( err ) console.log('ERROR: ' + err);
 });
+
+
+
+
 
 
 var content = fs.readFileSync('www/index.html').toString();
--- src/pages/policy/policy.ts
+++ src/pages/policy/policy.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { AppService } from '../../providers/app.service';
 @Component({
     selector: 'policy-page',
