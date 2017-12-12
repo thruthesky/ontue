a4767b25ecfb4732123d5cb82712e4aa4ed74177
JaeHo Song <thruthesky@gmail.com>
2017-12-12 23:39:53 +0900

    update for intro design. front text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -135,7 +135,7 @@ export class MyApp {
     // this.a.open('register');
 
     // this.a.open('register');
-    this.a.open('menu');
+    // this.a.open('menu');
 
     // this.a.open('reservation', { past: true });
 
