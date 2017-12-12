3a8e5c3bb1ea081bc2547efecf3f01c6a18ba3f5
JaeHo Song <thruthesky@gmail.com>
2017-12-12 16:46:19 +0900

    update for intro page design

---
+++ src/pages/intro/intro.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'intro-page',
+    templateUrl: 'intro.html'
+})
+
+export class IntroPage implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
