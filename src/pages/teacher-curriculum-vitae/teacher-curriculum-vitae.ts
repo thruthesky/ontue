d7650031b1b1fe6309a33494b2f80792ad2e689f
lancelynyrd <pinedaclp@gmail.com>
2017-12-14 17:18:03 +0800

    update on registration and CV

--- src/pages/teacher-curriculum-vitae/teacher-curriculum-vitae.ts
+++ src/pages/teacher-curriculum-vitae/teacher-curriculum-vitae.ts
@@ -1,7 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
 import {AppService} from './../../providers/app.service';
 import {
-  FILES, USER_UPDATE, USER_DATA_RESPONSE, USER_UPDATE_RESPONSE,
+  FILES, USER_UPDATE, USER_DATA_RESPONSE, USER_UPDATE_RESPONSE, FILE,
 } from './../../angular-xapi/interfaces';
 import {FileUploadWidget} from '../../components/file-upload/file-upload';
 
@@ -16,13 +16,21 @@ export class TeacherCurriculumVitaePage {
   account: USER_UPDATE = <USER_UPDATE>{};
 
   files: FILES = [];
+  qrmarks: FILES = [];
   @ViewChild('fileUploadWidget') fileUpload: FileUploadWidget;
+  @ViewChild('fileUploadWidgetQRMARK') fileUploadQRMark: FileUploadWidget;
 
-  constructor(public a: AppService) {
+  showQRMark: boolean = false;
 
-
-    if (a.user.isLogin) {
+  constructor(
+    public a: AppService
+  ) {
+    if (a.user.isLogin && a.isTeacher) {
       this.loadData();
+    }
+    else {
+      a.open('home');
+      a.alert("User type must be teacher and should login first...");
     }
 
 
@@ -46,7 +54,11 @@ export class TeacherCurriculumVitaePage {
       this.account.introduction = userData.introduction;
       this.account['display_name'] = userData['display_name'];
       this.account.kakaotalk_id = userData.kakaotalk_id;
-      if (userData.primary_photo.id) this.files[0] = userData.primary_photo;
+      if (userData.primary_photo.id) this.files = [ userData.primary_photo ];
+      if (userData.kakao_qrmark_photo.id){
+        this.qrmarks = [ userData.kakao_qrmark_photo ];
+        this.showQRMark = true;
+      }
     }, error => this.a.alert(error));
   }
 
@@ -54,7 +66,7 @@ export class TeacherCurriculumVitaePage {
     console.log("onSubmitUpdate", this.account);
     if (!this.account.name || !this.account.name.length) return this.a.showAlert(-80021, '*Name is required...');
     if (!this.account.user_email || !this.account.user_email.length) return this.a.showAlert(-80022, '*Email is required...');
-
+    if (!this.qrmarks.length ) return this.a.showAlert(-80025, '*Teacher must upload QR Mark...');
     // this.account.photoURL = this.files.length ? this.files[0].url : '';
 
     this.a.showLoader();
@@ -68,6 +80,77 @@ export class TeacherCurriculumVitaePage {
       this.a.hideLoader();
     });
   }
+
+
+  onSuccessUploadPicture(file: FILE) {
+    console.log("onSuccessUpdateProfilePicture::", this.files);
+
+
+    /**
+     * Delete previous photo.
+     *
+     * file[0]
+     */
+    if (this.files.length > 1) { /// If there are two files, one for prvious photo, the other is for new photo.
+      this.fileUpload.deleteFile(this.files[0], () => this.updatePrimaryPhoto(file), () => this.updatePrimaryPhoto(file));
+    }
+    else this.updatePrimaryPhoto(file);
+
+
+  }
+
+  updatePrimaryPhoto(file) {
+
+    let data: USER_UPDATE = {
+      photoURL: this.files[0].url,
+      user_email: this.account.user_email
+    };
+
+    if (this.a.user.isLogin) {
+      this.a.user.update(data).subscribe((res: USER_UPDATE_RESPONSE) => {
+
+        console.log("updatePrimaryPhoto", file);
+        this.files[0] = file;
+        this.a.render();
+      }, err => {
+        this.a.alert(err);
+      });
+    }
+
+  }
+
+
+
+
+  onSuccessUploadQRMark(file: FILE) {
+    this.showQRMark = false;
+    if (this.qrmarks.length > 1) {
+      this.fileUploadQRMark.deleteFile(this.qrmarks[0], () => {}, e => this.a.alert(e));
+    }
+
+    let data: USER_UPDATE = {
+      kakao_qrmark_URL: file.url,
+      user_email: this.account.user_email
+    };
+    this.a.user.update(data).subscribe(() => {
+      this.a.lms.update_kakao_qrmark_string().subscribe(re => {
+        console.log("update_kakao_qrmark_string",re);
+        if( !re.kakao_qrmark_string ) {
+          this.a.alert("Invalid QR MARK, Please try again");
+          this.fileUploadQRMark.deleteFile(this.qrmarks[0], () => {
+            this.qrmarks = [];
+            this.a.render();
+          }, e => this.a.alert(e));
+        }
+        else {
+          this.qrmarks = [file];
+          this.showQRMark = true;
+          this.a.render();
+        }
+      }, e => this.a.alert(e));
+    }, e => this.a.alert(e));
+  }
+
 
 
 }
