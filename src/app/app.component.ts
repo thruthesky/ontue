111d5ae8c1373bf7d6480aee4b5592b73408a474
JaeHo Song <thruthesky@gmail.com>
2017-12-27 00:09:27 +0800

    enabling cordova

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
 import { Platform, Nav } from 'ionic-angular';
-// import { StatusBar } from '@ionic-native/status-bar';
-// import { SplashScreen } from '@ionic-native/splash-screen';
-// import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
 
 
 import { XapiService } from './../angular-xapi/angular-xapi.module';
@@ -53,27 +53,21 @@ export class MyApp {
 
   constructor(
     platform: Platform,
-    // statusBar: StatusBar,
-    // splashScreen: SplashScreen,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
     xapi: XapiService,
     public a: AppService
     // private youtube: YoutubeVideoPlayer
   ) {
     this.a.initTranslate();
 
-    // platform.ready().then(() => {
-    //   // Okay, so the platform is ready and our plugins are available.
-    //   // Here you can do any higher level native things you might need.
-    //   // statusBar.styleDefault();
-    //   // splashScreen.hide();
-    //
-    //   this.a.platform = platform;
-    //
-    //   // this.youtube.openVideo('VbdfKmgpqjc');
-    //
-    //
-    //   // alert("I am on cordova!");
-    // });
+    platform.ready().then(() => {
+      
+      statusBar.styleDefault();
+      splashScreen.hide();
+      this.a.platform = platform;
+    
+    });
 
   }
 
