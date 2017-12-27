59ca7f2a9b4acaeeba0f9354f08c267ad82375ee
lancelynyrd <pinedaclp@gmail.com>
2017-12-27 14:58:22 +0800

    update on the session

--- src/components/session-list/session-list.ts
+++ src/components/session-list/session-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 import { AppService } from '../../providers/app.service';
 import { ModalController, NavParams} from 'ionic-angular';
 import { EvaluateView} from '../../components/evaluate-view/evaluate-view';
