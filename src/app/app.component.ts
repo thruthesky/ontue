b638c16aa5d7fcb1b7c8d1350f95c1868cbdf49a
lancelynyrd <pinedaclp@gmail.com>
2018-01-24 19:16:17 +0800

    minor fix on CV

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { Platform, Nav } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+// import { StatusBar } from '@ionic-native/status-bar';
+// import { SplashScreen } from '@ionic-native/splash-screen';
 
 
 
@@ -67,8 +67,8 @@ export class MyApp {
 
   constructor(
     platform: Platform,
-    statusBar: StatusBar,
-    splashScreen: SplashScreen,
+    // statusBar: StatusBar,
+    // splashScreen: SplashScreen,
     xapi: XapiService,
     public a: AppService
   ) {
@@ -76,8 +76,8 @@ export class MyApp {
 
     platform.ready().then(() => {
       this.a.platform = platform;
-      statusBar.styleDefault();
-      splashScreen.hide();
+      // statusBar.styleDefault();
+      // splashScreen.hide();
     });
 
   }
@@ -180,7 +180,7 @@ export class MyApp {
     // this.a.open('login');
 
     // this.a.open('register');
-    
+
     // this.a.open('menu');
 
     // this.a.open('reservation', {past: true})
