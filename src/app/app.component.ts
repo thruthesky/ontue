72d2e1e9baf3877ef03e9ae12edb9770512aaf5d
JaeHo Song <thruthesky@gmail.com>
2018-01-27 20:18:23 +0800

    update on payment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -204,7 +204,7 @@ export class MyApp {
 
     // this.a.open('payment');
     // this.a.open('payment-result', { result: false, message: '알수없는 에러'});
-    this.a.open('payment-result', { result: true, message: ''});
+    // this.a.open('payment-result', { result: true, message: ''});
 
     // this.a.open('policy');
     // this.a.open('forum');
