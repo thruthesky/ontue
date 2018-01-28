3986baff7ca00a6af509f22d63f2a17f40573cc6
JaeHo Song <thruthesky@gmail.com>
2018-01-28 17:48:57 +0800

    update for past

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { Platform, Nav } from 'ionic-angular';
-// import { StatusBar } from '@ionic-native/status-bar';
-// import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 
 
@@ -70,8 +70,8 @@ export class MyApp {
 
   constructor(
     platform: Platform,
-    // statusBar: StatusBar,
-    // splashScreen: SplashScreen,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
     xapi: XapiService,
     public a: AppService
   ) {
@@ -79,8 +79,8 @@ export class MyApp {
 
     platform.ready().then(() => {
       this.a.platform = platform;
-      // statusBar.styleDefault();
-      // splashScreen.hide();
+      statusBar.styleDefault();
+      splashScreen.hide();
     });
 
   }
@@ -222,8 +222,8 @@ export class MyApp {
 
     // this.a.open('home');
 
-    // this.a.open('session-past');
-    this.a.open('session-future');
+    this.a.open('session-past');
+    // this.a.open('session-future');
     // this.a.open('password-change');
 
     // this.a.open('schedule-available');
