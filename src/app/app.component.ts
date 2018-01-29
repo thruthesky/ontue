84e8e43b9f924db21f93c5c5f19df2b740e6354b
JaeHo Song <thruthesky@gmail.com>
2018-01-29 19:26:39 +0800

    update on design. Uploading 1.0.26

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { Platform, Nav } from 'ionic-angular';
-// import { StatusBar } from '@ionic-native/status-bar';
-// import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 
 
@@ -70,8 +70,8 @@ export class MyApp {
 
   constructor(
     platform: Platform,
-    // statusBar: StatusBar,
-    // splashScreen: SplashScreen,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
     xapi: XapiService,
     public a: AppService
   ) {
@@ -79,8 +79,8 @@ export class MyApp {
 
     platform.ready().then(() => {
       this.a.platform = platform;
-      // statusBar.styleDefault();
-      // splashScreen.hide();
+      statusBar.styleDefault();
+      splashScreen.hide();
     });
 
   }
@@ -167,7 +167,7 @@ export class MyApp {
 
     // this.a.open('schedule-table', { ID: 809 }); // one schedule, Imno4
     // this.a.open('schedule-table', { ID: 969 }); // many schedule
-    this.a.open('schedule-table', { ID: 806 }); /// tabetha
+    // this.a.open('schedule-table', { ID: 806 }); /// tabetha
     // this.a.open('schedule-table', { ID: 945 }); /// Teacher
 
 
