d6caa5c94f24ea29a34a14a8f21e17e7cb0fbe82
lancelynyrd <pinedaclp@gmail.com>
2018-02-01 23:45:57 +0800

    disable john work

--- copy.js
+++ copy.js
@@ -1,3 +1,3 @@
 var fs = require('fs-extra');
 fs.copy('src/firebase-messaging-sw.js','www/firebase-messaging-sw.js');
-fs.copy('src/manifest.json','www/manifest.json');
+//fs.copy('src/manifest.json','www/manifest.json');
