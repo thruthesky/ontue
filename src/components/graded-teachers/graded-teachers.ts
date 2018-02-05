1664ed16c8541624895b7e3fcb85a727c3c182c1
JMMgit <jm.muan@gmail.com>
2018-02-05 20:59:30 +0800

    updated teacher homepage

--- src/components/graded-teachers/graded-teachers.ts
+++ src/components/graded-teachers/graded-teachers.ts
@@ -1,13 +1,11 @@
-import { Component, Input, OnInit  } from '@angular/core';
+import {Component, Input, OnInit  } from '@angular/core';
 import { AppService } from './../../providers/app.service';
-
 
 @Component({
   selector: 'graded-teachers-component',
   templateUrl: 'graded-teachers.html'
 })
 export class GradedTeachersComponent implements OnInit  {
-
   @Input() teachers = [];
 
   constructor(
--- src/components/teacher-grading-system/teacher-grading-system.ts
+++ src/components/teacher-grading-system/teacher-grading-system.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { ViewChild,Component } from '@angular/core';
 import { AppService } from './../../providers/app.service';
-
+import { Slides } from 'ionic-angular';
 
 @Component({
   selector: 'teacher-grading-system-component',
   templateUrl: 'teacher-grading-system.html'
 })
 export class TeacherGradingSystemComponent {
-
+  @ViewChild('gradingSystem') slides: Slides;
   constructor(
       public a: AppService
     ) {
+  }
 
+  next() {
+      this.slides.slideNext(500);
+  }
+
+  previous() {
+    this.slides.slidePrev(500);
   }
 }
