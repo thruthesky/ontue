f572ca400a067f619bf1f7532d32c9333953748b
JMMgit <jm.muan@gmail.com>
2018-02-06 20:46:10 +0800

    update teacher homepage

--- src/components/teacher-reminders/teacher-reminders.ts
+++ src/components/teacher-reminders/teacher-reminders.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { AppService } from './../../providers/app.service';
-import {ModalController} from "ionic-angular";
-import {HowToInstallKakaoComponent} from "../how-to-install-kakao/how-to-install-kakao";
 
 
 @Component({
@@ -12,14 +10,6 @@ export class TeacherRemindersComponent {
 
   constructor(
       public a: AppService,
-      public modalCtrl: ModalController
     ) {
   }
-
-  showKakaoInstall(){
-    const modal = this.modalCtrl.create( HowToInstallKakaoComponent );
-    modal.onDidDismiss(()=> {});
-    modal.present();
-  }
-  
 }
--- src/components/teacher-steps/teacher-steps.ts
+++ src/components/teacher-steps/teacher-steps.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AppService } from './../../providers/app.service';
-
+import {HowToInstallKakaoComponent} from "../how-to-install-kakao/how-to-install-kakao";
+import {ModalController} from "ionic-angular";
 
 @Component({
   selector: 'teacher-steps-component',
@@ -9,8 +10,15 @@ import { AppService } from './../../providers/app.service';
 export class TeacherStepComponent {
 
   constructor(
-      public a: AppService
+      public a: AppService,
+      public modalCtrl: ModalController
     ) {
 
+  }
+
+  showKakaoInstall(){
+    const modal = this.modalCtrl.create( HowToInstallKakaoComponent );
+    modal.onDidDismiss(()=> {});
+    modal.present();
   }
 }
