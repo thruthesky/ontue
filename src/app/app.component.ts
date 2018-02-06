d0982f11d09337810186aa31a446475a0e1e004f
JaeHo Song <thruthesky@gmail.com>
2018-02-06 21:33:06 +0800

    update for cpoyrgith

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -239,7 +239,7 @@ export class MyApp {
     // this.a.open('home');
 
     // this.a.open('session-past');
-    // this.a.open('session-future');
+    this.a.open('session-future');
     // this.a.open('password-change');
 
     // this.a.open('schedule-available');
