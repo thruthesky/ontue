18d890b2f91cb8c360aeacfe49913354b08cc24e
JaeHo Song <thruthesky@gmail.com>
2018-02-13 12:49:05 +0800

    update on app timezone

--- src/providers/app.service.ts
+++ src/providers/app.service.ts
@@ -39,8 +39,8 @@ export class AppService {
     countries = ["Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"];
 
 
-    urlBackend = "https://sonub.com:8443";
-    // urlBackend = "https://www.ontue.com";
+    // urlBackend = "https://sonub.com:8443";
+    urlBackend = "https://www.ontue.com";
 
 
     NO_SCHEDULE_PER_PAGE = 50;
@@ -433,18 +433,18 @@ export class AppService {
      * 
      * @endcode
      */
-    okDialog( title, message, callback = null ) {
+    okDialog(title, message, callback = null) {
 
         let close = { text: this.i18n['CONFIRM'] };
-        if ( callback ) close['handler'] = () => callback();
+        if (callback) close['handler'] = () => callback();
         let alert = this.alertCtrl.create({
             title: title,
             subTitle: message,
-            buttons: [ close ],
+            buttons: [close],
             enableBackdropDismiss: false
-          });
+        });
 
-          alert.present();
+        alert.present();
     }
 
     /**
@@ -923,8 +923,8 @@ export class AppService {
     }
 
 
-    onLmsReserve( teacher_name ) {
-        if ( ! teacher_name ) return;
+    onLmsReserve(teacher_name) {
+        if (!teacher_name) return;
         this.log({ idx_user: this.user.id, name: this.user.name, activity: 'reserve', target: teacher_name });
     }
     /**
@@ -936,8 +936,8 @@ export class AppService {
         this.log({ idx_user: this.user.id, name: this.user.name, activity: 'cancel', target: teacher_name });
     }
 
-    onUserViewProfile( teacher_name ) {
-        if ( ! teacher_name ) return;
+    onUserViewProfile(teacher_name) {
+        if (!teacher_name) return;
         this.log({ idx_user: this.user.id, name: this.user.name, activity: 'view-profile', target: teacher_name });
     }
     onBeginPayment() {
@@ -1018,16 +1018,19 @@ export class AppService {
     }
 
 
-    translateTimezoneCountry( cname ) {
-
-      /**
-       * @todo @attention This translation should be done in 
-       */
-      if (this.isKorean) {
-        if ( cname == 'Philippines, China') return '필리핀, 중국';
-        else if ( cname == 'Korea, Japan' ) return '한국, 일본';
-      }
-      return cname;
+    translateTimezoneCountry(cname) {
+        /**
+         * @todo @attention This translation should be done in 
+         */
+        if (this.isKorean) {
+            if ( !cname ) return '(시간대 없음)';
+            else if (cname == 'Philippines, China') return '필리핀, 중국';
+            else if (cname == 'Korea, Japan') return '한국, 일본';
+        }
+        else {
+            if ( ! cname ) return '(No timezone)';
+            return cname;
+        }
 
     }
 
