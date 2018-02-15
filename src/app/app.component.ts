b24f9549cd037294677f22c5db370f1a813fe275
JaeHo Song <thruthesky@gmail.com>
2018-02-15 21:21:38 +0800

    update for teacher list options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -199,7 +199,7 @@ export class MyApp {
     // this.a.open('schedule-table', { ID: 137 }); /// teacher21
     // this.a.open('schedule-table', { ID: 138 }); /// teacher22
 
-    // this.a.open('teacher-list');
+    this.a.open('teacher-list');
 
     // this.a.open('teacher-list', {mode: 'leveltest'});
 
