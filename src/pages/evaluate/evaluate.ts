6d4e658ef1bc138f63fa71bd0b6bfe9ef3c98504
JMMgit <jm.muan@gmail.com>
2018-03-20 19:44:06 +0800

    update evaluate design

--- src/pages/evaluate/evaluate.ts
+++ src/pages/evaluate/evaluate.ts
@@ -19,7 +19,7 @@ export class EvaluatePage {
   grammar: number = 0;
   pronunciation: number = 0;
   speed: number = 0;
-  comment: string;
+  comment: string="d";
   book_used: string;
   book_next: string;
   student_name:string;
