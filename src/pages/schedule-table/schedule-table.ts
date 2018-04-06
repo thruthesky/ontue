ef6a99ef2cc8cf8ac7cfe1fcbb3ab9b8f1a053b6
jmmgit <jm.muan@gmail.com>
2018-04-06 18:46:03 +0800

    update

--- src/pages/schedule-table/schedule-table.ts
+++ src/pages/schedule-table/schedule-table.ts
@@ -105,7 +105,7 @@ export class ScheduleTablePage {
   student = {}; // Get's login user information from backend. it is more accurate than localStorage.
   teacher_profile = { age: 0, gender: '', name: '', idx: 0, photoURL: '', grade: 0, total_reservation: 0, kakao_qrmark_string: '', introduction: '' };
   teachers = {};
-  // enableSticky=false;
+  enableSticky=false;
 
 
   no_more_schedule = false;
@@ -870,15 +870,15 @@ export class ScheduleTablePage {
     else this.a.alert('앗, 이 선생님의 카카오톡을 입력하지 않았습니다.');
   }
 
-  // checkSticky(){
-  //   let elTop=document.querySelector('.scroll-content').getBoundingClientRect().top;
-  //   let targetTop=document.getElementById('schedule-header').getBoundingClientRect().top;
+  checkSticky(){
+    let elTop=document.querySelector('.scroll-content').getBoundingClientRect().top;
+    let targetTop=document.getElementById('schedule-header').getBoundingClientRect().top;
 
-  //   if(elTop>=targetTop-1){
-  //     this.enableSticky=true;
-  //   }else{
-  //     this.enableSticky=false;
-  //   }
-  // }
+    if(elTop>=targetTop-1){
+      this.enableSticky=true;
+    }else{
+      this.enableSticky=false;
+    }
+  }
 
 }
