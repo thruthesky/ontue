52aafd126f080ebbd49c42689b33e0b4a28fae5e
lancelynyrd <pinedaclp@gmail.com>
2018-05-21 17:10:03 +0800

    update on salary computaion

--- src/pages/salary-computation/salary-computation.ts
+++ src/pages/salary-computation/salary-computation.ts
@@ -45,6 +45,7 @@ export class SalaryComputationPage implements OnInit {
 
 
     this.a.lms.get_payment_computation_info().subscribe(re => {
+      console.log('get_payment_computation_info', re);
       this.payment_computation = re;
       this.recompute();
     }, e => {
@@ -59,13 +60,19 @@ export class SalaryComputationPage implements OnInit {
     this.teacher_share = this.total_points * this.payment_computation.teacher_share / 100;
     this.paypal_charges = this.teacher_share * this.payment_computation.transaction_fee / 100;
     if ( this.payment_information['payment_method'] !== 'paypal' ) {
+      console.log('this.payment_information[payment_method]', this.payment_information['payment_method']);
       this.buying_rate = this.teacher_share * this.payment_computation.buyer_rate / 100;
     }
     this.earnings = Math.round(this.teacher_share - this.paypal_charges - this.buying_rate);
     if (this.payment_information['payment_method'] === 'paypal') {
       this.salary = Math.round(this.earnings / parseInt(this.payment_computation['usd'])) + 'USD';
     } else {
-      let converted =  Math.round(this.earnings / parseInt(this.payment_computation['php']));
+
+      console.log('this.earnings',  this.earnings);
+      console.log('his.payment_computation[php]',  this.payment_computation['php']);
+      let converted =  Math.round(this.earnings / parseFloat(this.payment_computation['php']));
+
+      console.log('converted',  converted);
       this.salary = converted + 'PHP';
     }
 
